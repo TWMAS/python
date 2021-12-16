d882ec264e2d5a2421b9c44386186ce06d14e071
TWMAS <93318670+TWMAS@users.noreply.github.com>
2021-12-16 11:43:06 +0100

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -1,7 +1,5 @@
-x = 4
-if x >= 0:
-    print("x es positivo.")
+entrada_usuario = int(input("x:"))
+if entrada_usuario >= 0:
+    print("x es positiu")
 else:
-    print("x no es positivo.")
-
-
+    print("x no es positiu")
